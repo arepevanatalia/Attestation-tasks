19ad021f48fb5ea755638e1b761c6c0c80d9eb83
arepevanatalia <152387360+arepevanatalia@users.noreply.github.com>
2023-12-20 22:45:21 +0900

    Add tasks
    Add tasks

---
+++ Tasks/Task001/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 1.Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы.
+
+using System;
+class Program
+{
+        static void Main(string[] args)
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        PrintNumbers(m, n);
+    }
+
+    static void PrintNumbers(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.WriteLine(m);
+            PrintNumbers(m + 1, n);
+        }
+    }
+}
+
---
+++ Tasks/Task002/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение m: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int result = Ackermann(m, n);
+        Console.WriteLine("Результат: " + result);
+    }
+
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (m > 0 && n == 0)
+        {
+            return Ackermann(m - 1, 1);
+        }
+        else
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
+}
